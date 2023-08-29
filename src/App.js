736950199de2ab67c6a244853fafef101bcd900e
fuseeiam <nattaporn.orr@gmail.com>
2023-08-29 15:21:48 +0700

    finish-02-layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './App.scss';
+
 function App() {
   return (
-    <div className="App">
-      React-App
+    <div className="todo">
+      <div className="todo__header">Header</div>
+      <div className="todo__sidebar">SideBar</div>
+      <div classNAme="todo__content">TodoContent</div>
     </div>
   );
 }
