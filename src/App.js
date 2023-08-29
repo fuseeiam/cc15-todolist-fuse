75fc0f73aa013c9416e35bb27e28dea4477d7598
fuseeiam <nattaporn.orr@gmail.com>
2023-08-29 15:26:23 +0700

    finish-03-background-color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="todo">
       <div className="todo__header">Header</div>
       <div className="todo__sidebar">SideBar</div>
-      <div classNAme="todo__content">TodoContent</div>
+      <div className="todo__content">TodoContent</div>
     </div>
   );
 }
