ebc413b649466b684a86d43e5fe2f6c955a8ce29
fuseeiam <nattaporn.orr@gmail.com>
2023-08-31 09:35:09 +0700

    finish: all Ul

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,24 +1,27 @@
 import styles from './TodoLists.module.scss';
-import { FaTrashAlt, FaPen, FaRegCircle } from 'react-icons/fa';
+import { FaTrashAlt, FaPen } from 'react-icons/fa';
+import { HiOutlineCheck } from 'react-icons/hi';
 
 function TodoLists() {
-    return (
-        <ul className={styles.todo__lists}>
-            <li className={styles.todo}>
-                <span className={styles.todo__checkbox}>{/* <FaRegCircle /> */}</span>
-                <p className={styles.todo__task}>TodoItem 1 </p>
-                <span className={styles.todo__date}>30 AUG</span>
-                <div className={styles.todo__action}>
-                    <span className={styles.todo__edit}>
-                        <FaPen />
-                    </span>
-                    <span className={styles.todo__delete}>
-                        <FaTrashAlt />
-                    </span>
-                </div>
-            </li>
-        </ul>
-    );
+  return (
+    <ul className={styles.todo__lists}>
+      <li className={styles.todo}>
+        <div className={styles.todo__checkbox}>
+          <HiOutlineCheck className={styles.todo__checkbox__icon} />
+        </div>
+        <p className={styles.todo__task}>todo-item 1 </p>
+        <span className={styles.todo__date}>30 Aug</span>
+        <div className={styles.todo__action}>
+          <span>
+            <FaPen className={styles.todo__edit} />
+          </span>
+          <span>
+            <FaTrashAlt className={styles.todo__delete} />
+          </span>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default TodoLists;
